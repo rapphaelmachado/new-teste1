9bbfb2b91d4938e45648ee8dea56d31c017bd27b
rapphaelmachado <104630138+rapphaelmachado@users.noreply.github.com>
2022-09-11 03:53:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+let count1 = 11;
+document.getElementById("radio11").checked = true;
+
+setInterval( function(){
+    nextImage2();
+}, 4000)
+
+function nextImage2(){
+    count1++;
+    if(count1>30){
+       count1 = 11;
+    }
+
+    document.getElementById("radio"+count1).checked = true;
+}
